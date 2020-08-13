5a27e3e9af975b69d159920c4f7cdf847be0386d
maxrfinch <45885290+maxrfinch@users.noreply.github.com>
2020-08-13 16:00:13 -0700

    CTA now saves

--- script.js
+++ script.js
@@ -217,9 +217,9 @@ $('.main').on('click', '.element',function(e){
     } else if (['h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'a','p'].includes(e.target.localName)) {
       if(e.target.localName == 'a') {
         e.target.innerText = prompt('Enter Call To Action Text') + " →";
-        return true
+      } else {
+        e.target.innerText = prompt('Enter Heading Text Here');
       }
-      e.target.innerText = prompt('Enter Heading Text Here');
     }
   } else {
     e.target.remove();
